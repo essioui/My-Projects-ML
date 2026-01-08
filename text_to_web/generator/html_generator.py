dfd4d57a13dce090288d3fa8d403351281dca664
essioui <salahessioui@gmail.com>
2026-01-08 01:09:24 +0100

    css

--- text_to_web/generator/html_generator.py
+++ text_to_web/generator/html_generator.py
@@ -17,11 +17,50 @@ def load_component(section, name):
     return path.read_text()
 
 
+def style_dict_to_css(style_dict):
+    """
+    """
+    css = ""
+    if not style_dict:
+        return css
+    
+    if "align" in style_dict:
+        css += f"text-align: {style_dict['align']}; "
+        
+    if "color" in style_dict:
+        css += f"color: {style_dict['color']}; "
+    
+    if "background_color" in style_dict:
+        css += f"background-color: {style_dict['background_color']}; "
+    
+    if "font_size" in style_dict:
+        css += f"font-size: {style_dict['font_size']}; "
+    
+    if "size" in style_dict:
+        if style_dict["size"] == "large":
+            css += "font-size:32px;"
+        elif style_dict["size"] == "small":
+            css += "font-size:12px;"
+
+    return f' style="{css}"'
+
+
 def render_component(section, el):
     """
     Render a component with its data.
     """
     template = load_component(section, el["type"])
+    
+    style_css = style_dict_to_css(el.get("style", {}))
+    el = el.copy()
+    el["style"] = style_css
+    
+    if el["type"] == "navbar":
+        items_html = ""
+        for item in el.get("items", []):
+            items_html += f"<li>{item}</li>\n"
+        el["items"] = items_html
+
     return template.format(**el)
 
 
